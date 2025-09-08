2d63b94103ab0ebfdbe1d515658b0322a1d0117c
CoolShivas <shivas2710cool00@gmail.com>
2025-09-08 12:20:01 +0530

    Use of Bootstrap's Include via CDN for CSS.

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,9 +19,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body className="bg-dark text-light">{children}</body>
     </html>
   );
 }
