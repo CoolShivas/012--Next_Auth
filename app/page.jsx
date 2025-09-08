db10a972893458e8eebc8a333f39fb4c10bff114
CoolShivas <shivas2710cool00@gmail.com>
2025-09-08 12:17:17 +0530

    Some of the changes done before starting the project.

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <center>
+        <h1>Welcome to the Home Page.</h1>
+      </center>
+    </>
+  );
+};
+
+export default HomePage;
